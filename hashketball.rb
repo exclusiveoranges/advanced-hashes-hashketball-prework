98cc28c517308781148d928f76edaaf0c0d4d89b
Ryan O'Toole <otoolerj@gmail.com>
2018-03-26 21:22:09 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -121,7 +121,7 @@ end
 def num_points_scored(player_name)
   home_players = game_hash[:home][:players]
   away_players = game_hash[:away][:players]
-
+  all_players = home_players + away_players
   binding.pry
   if player_name == "Alan Anderson" ||
     player_name == "Reggie Evans" ||
