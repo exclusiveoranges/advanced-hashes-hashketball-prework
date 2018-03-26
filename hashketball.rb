f27633e528df88b41e823e371504f9d22438cf45
Ryan O'Toole <otoolerj@gmail.com>
2018-03-26 21:29:43 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -125,3 +125,10 @@ def num_points_scored(player_name)
   player = all_players[player_name]
   player[:points]
 end
+
+def find_player
+  home_players = game_hash[:home][:players]
+  away_players = game_hash[:away][:players]
+  all_players = home_players.merge(away_players)
+  player = all_players[player_name]
+end
